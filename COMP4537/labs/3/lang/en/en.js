84d75927b2efdb8751ba55674b6be85014c386c7
Evin <evin4369@gmail.com>
2025-02-01 19:25:16 -0800

    Added code to display the page with the correct information

---
+++ COMP4537/labs/3/lang/en/en.js
@@ -0,0 +1,7 @@
+const msg = {
+    greeting: "Hello %1, What a beautiful day. Server current date and time is * ",
+    open_tag: "<div style = \"color:blue\">",
+    close_tag: "</div>"
+}
+
+module.exports = {msg};
--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -1,10 +1,21 @@
 let http = require("http");
+let url = require("url");
+
+let adr = ''
 
 let dt = require("./modules/utils");
+let text = require("./lang/en/en");
 
 http.createServer(function (req, res) {
+
+    const query = url.parse(req.url, true).query;
+    const name = query.name || "Guest";
+
     res.writeHead(200, {'Content-Type': 'text/html'});
-    // res.write("Now: " + dt.Date());
-    res.write("Now: ");
+
+    const message = text.msg.greeting.replace("%1", name);
+    res.write(text.msg.open_tag + message + dt.getDate() + text.msg.close_tag);
     res.end();
 }).listen(8888);
+
+console.log("Listening on 8888");
