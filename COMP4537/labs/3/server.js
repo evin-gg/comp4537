4f9840b30a97a6eebab92a1bf7bc18aa98652067
Evin <evin4369@gmail.com>
2025-02-02 02:26:53 -0800

    added an routing so that the link looks better

--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -1,21 +1,25 @@
 let http = require("http");
 let url = require("url");
-
-let adr = ''
-
 let dt = require("./modules/utils");
 let text = require("./lang/en/en");
 
 http.createServer(function (req, res) {
 
-    const query = url.parse(req.url, true).query;
-    const name = query.name || "Guest";
+    const parsedUrl = url.parse(req.url, true);
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    const propertyUrl = parsedUrl.query;
+    const name = propertyUrl.name || "Guest";
 
-    const message = text.msg.greeting.replace("%1", name);
-    res.write(text.msg.open_tag + message + dt.getDate() + text.msg.close_tag);
-    res.end();
+    if (parsedUrl.pathname === "/COMP4537/labs/3/getDate/") {
+        const message = text.msg.greeting.replace("%1", name);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(text.msg.open_tag + message + dt.getDate() + text.msg.close_tag);
+        res.end();
+    }
+
+    
+
+    
 }).listen(8888);
 
 console.log("Listening on 8888");
